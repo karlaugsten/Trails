324e0caa60f4d94ff2d01c858c3a63f1c2295a52
Karl Augsten <karlaug@microsoft.com>
2019-01-26 18:44:54 -0700

    Fix login modal to properly animate when requested.

--- trails/src/Components/LoginModal.js
+++ trails/src/Components/LoginModal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from "react-dom";
 import {Modal, Button} from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
@@ -10,11 +11,30 @@ class LoginModal extends Component {
     super(props);
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      show: false,
+      top: 0,
+      left: 0
     }
   }
 
   componentDidMount() {
+    
+  }
+
+  componentDidUpdate(prevProps) {
+    if(this.props.loginRequested && !prevProps.loginRequested) {
+      // Calculate width and height of modal and center it on screen.
+      const height = ReactDOM.findDOMNode(this.node_modal).clientHeight;
+      const width = ReactDOM.findDOMNode(this.node_modal).clientWidth;
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+      this.setState({
+          top: Math.max((windowHeight - height) / 2, 0),
+          left: Math.max((windowWidth - width) / 2, 0),
+      });
+      setTimeout(() => this.setState({ show: true }), 100);
+    }
   }
 
   handleClose() {
@@ -35,39 +55,36 @@ class LoginModal extends Component {
 
   render() {
     const { loginRequested, loginError, loggedIn } = this.props;
-    if (!loginRequested) {
-      return null;
-    }
     let loginErrorMessage;
     if (loginError) {
       loginErrorMessage = (
         <p>An error occurred. {loginError}</p>
       );
     }
-    if(loginRequested && !loggedIn) {
-      return (
-      <React.Fragment>
-      <div className="login-modal-overlay" />
-      <div className="login-modal" onHide={this.handleClose.bind(this)}>
-        <Modal.Header closeButton onHide={this.handleClose.bind(this)}>
-          <Modal.Title>You must login.</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {loginErrorMessage}
-          <form>
-            <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange.bind(this)} />
-            <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange.bind(this)} />
-          </form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button class="pull-left" onClick={this.handleLogin.bind(this)}>Login</Button>
-          <Button class="pull-right" onClick={this.handleClose.bind(this)}>Cancel</Button>
-        </Modal.Footer>
-      </div>
-      </React.Fragment>
-      );
-    }
-    return null;
+
+    return (
+    <React.Fragment>
+    <div className={loginRequested && !loggedIn ? "login-modal-overlay" : ""} />
+    <div ref={node => {
+                      this.node_modal = node;
+                  }} style={{ transform: loginRequested && !loggedIn ? null : "translateY(-1500px)", top: this.state.top, left: this.state.left }} className="login-modal" onHide={this.handleClose.bind(this)}>
+      <Modal.Header closeButton onHide={this.handleClose.bind(this)}>
+        <Modal.Title>You must login.</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {loginErrorMessage}
+        <form>
+          <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange.bind(this)} />
+          <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange.bind(this)} />
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button class="pull-left" onClick={this.handleLogin.bind(this)}>Login</Button>
+        <Button class="pull-right" onClick={this.handleClose.bind(this)}>Cancel</Button>
+      </Modal.Footer>
+    </div>
+    </React.Fragment>
+    );
   }
 }
 
