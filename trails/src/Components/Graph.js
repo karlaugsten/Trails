0961d20cc3a38f3f24a478c01a39c1f720b62ec5
Karl Augsten <karlaugsten@gmail.com>
2020-03-02 22:22:42 -0800

    Trying ZingChart

--- trails/src/Components/Graph.js
+++ trails/src/Components/Graph.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ZingChart from 'zingchart-react';
 
 export default class Graph extends React.Component {
     constructor(props) {
@@ -37,18 +38,109 @@ export default class Graph extends React.Component {
     }
 
     render() {
-        var renderedPoints = this.state.points.map((p, i) => {
-            return <div key={i} className="graph-point" style={{top: p.y, left: p.x}} />
-        })
-        var renderedLines = [...Array(20).keys()].map((p,i) => {
-            if(p > 0) {
-                return <div key={i} className="graph-y-line" style={{left: `${p*5}%`}} />
-            }
-        });
+
+        let myConfig = {
+            type: 'area',
+            backgroundColor:'none',
+            borderColor: 'none',
+            tooltip: {
+                //backgroundColor: 'rgba(0,0,0,0)',
+                fontColor: 'var(--fontColor)',
+                callout: true,
+                padding: '24px 56px',
+                borderRadius: 4,
+                borderWidth: 1,
+                borderColor: 'var(--lightGray)',
+                text: '$%v<br>October 13, %kl'
+            },
+         
+            scaleX: {
+                minValue: 0,
+                step: 100,
+                lineWidth: 0,
+                tick: {
+                    visible: false
+                },
+                item: {
+                    fontSize: '10px',
+                    //alpha: '0.4',
+                    fontColor: "rgb(233, 232, 232)"
+                },
+                label: { 
+                    text: "Distance (km)",
+                    borderColor: 'none',
+                    fontColor: 'rgb(233, 232, 232)',
+                    fontSize: 14,
+                    fontStyle: 'normal',
+                    fontWeight: 'normal',
+                    padding: '5px 20px'
+                        
+                }
+            },
+            scaleY: {
+                guide: {
+                    lineStyle: 'solid',
+                    lineColor: 'var(--gray)'
+                },
+                lineWidth: 0,
+                item: {
+                    fontSize: '10px',
+                    //alpha: '0.4'
+                    fontColor: "rgb(233, 232, 232)"
+                },
+                tick: {
+                    visible: false
+                },
+                values: '1000:0',
+                label: { 
+                    text: "Elevation (m)",
+                    borderColor: 'none',
+                    fontColor: 'rgb(233, 232, 232)',
+                    fontSize: 14,
+                    fontStyle: 'normal',
+                    fontWeight: 'normal',
+                    padding: '5px 20px'
+                }
+            },
+            'crosshair-x': {
+                'plot-label': {
+                    text: "%v meters",
+                    decimals: 1
+                }
+            },
+            plot: {
+                aspect: 'spline',
+                tooltip: { visible: false }
+                // Animation docs here:
+                // https://www.zingchart.com/docs/tutorials/styling/animation#effect
+                /*animation: {
+                    effect: 'ANIMATION_EXPAND_BOTTOM',
+                    method: 'ANIMATION_STRONG_EASE_OUT',
+                    sequence: 'ANIMATION_BY_NODE',
+                    speed: 275,
+                }*/
+            },
+            plotarea: {
+                backgroundColor: 'transparent'
+            },
+            series: [
+                {
+                    // plot 1 values, linear data
+                    values: this.state.points.map(p => p.y),
+                    backgroundColor: '#3E84F9 #fff',
+                    lineColor: 'var(--darkBlue)',
+                    lineWidth: 3,
+                    alpha: '0.7',
+                    marker: {
+                    visible: false
+                    }
+                }
+            ]       
+        };
+
         return (
             <div className="graph-wrapper">
-            {renderedPoints}
-            {renderedLines}
+                <ZingChart data={myConfig} />
             </div>
         );
     }
