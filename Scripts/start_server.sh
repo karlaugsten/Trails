2569d8d81d749a8bc7c4023c79705c5b430ad1a4
Karl Augsten <karlaugsten@gmail.com>
2020-08-10 00:00:25 -0700

    Add code deploy scripts

---
+++ Scripts/start_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Restoring dependencies..."
+sudo /usr/bin/dotnet restore
+
+echo "Updating database..."
+/usr/bin/dotnet ef database update
+
+echo "Building source code..."
+sudo /usr/bin/dotnet publish --configuration Release
+
+echo "Starting server..."
+sudo systemctl start runnify.service
---
+++ Scripts/stop_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Stopping server"
+sudo systemctl stop runnify.service
